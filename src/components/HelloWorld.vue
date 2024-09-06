26808c6cf917dcb3feea5909aa293dabc2df60b0
Wojciech Król <wojciech.krol@adminbrothers.com>
2024-09-06 15:06:44 +0200

    kalkulator

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,21 +1,62 @@
 <script setup lang="ts">
 import { ref } from "vue";
 
+const result = ref(0);
+
+const count1 = ref(0);
+const count2 = ref(0);
+const operation = ref("+");
+
+const options = ["+", "-", "*", "/"];
+
 defineProps<{ msg: string }>();
 
-const count = ref(0);
+function count(count1: number, count2: number, operation: string) {
+  switch (operation) {
+    case "+":
+      result.value = count1 + count2;
+      break;
+    case "-":
+      result.value = count1 - count2;
+      break;
+    case "*":
+      result.value = count1 * count2;
+      break;
+    case "/":
+      result.value = parseFloat((count1 / count2).toFixed(2));
+      break;
+    default:
+      result.value = 0;
+  }
+}
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
-
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <q-input
+      v-model.number="count1"
+      type="number"
+      filled
+      @change="count(count1, count2, operation)"
+    />
+
+    <q-select
+      filled
+      v-model="operation"
+      :options="options"
+      label="Filled"
+      @update:model-value="count(count1, count2, operation)"
+    />
+
+    <q-input
+      v-model.number="count2"
+      type="number"
+      filled
+      @change="count(count1, count2, operation)"
+    />
+    <q-input v-model.number="result" type="number" :readonly="true" />
   </div>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
+<style scoped></style>
